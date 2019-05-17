7280b20437a830f85798e447d38ae32871f04cf2
jinCom <jinhyun0504@hanmail.net>
2019-05-17 13:49:27 +0900

    vue test project

---
+++ testProject/todolistapp/src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+// import App from './App.vue'
+import TodoList from './components/TodoList.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+    render: h => h(TodoList),
+}).$mount('#app')
