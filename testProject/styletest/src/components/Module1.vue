c69a1590505a60175981a0fd7e8f9e77dbeb2475
jinhyun <jinhyun0504@hanmail.net>
2019-05-20 02:29:28 +0900

    19.05.19 test

---
+++ testProject/styletest/src/components/Module1.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button :class="$style.hand">CSS 모듈을 적용한 버튼 </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        created() {
+            /* eslint-disable */
+            console.log(this.$style);
+            console.log(this.$style);
+        }
+    }
+</script>
+
+<style module>
+    .hand { cursor:pointer; background-color: purple; color:yellow;}
+</style>
